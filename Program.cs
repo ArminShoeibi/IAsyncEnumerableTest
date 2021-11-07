b8e2df6d1a9015e9a84fe92f09e081d65e90e49a
armin <a.shoeibi@alibaba.ir>
2021-11-07 18:50:16 +0330

    delete unnecessary things

--- Program.cs
+++ Program.cs
@@ -8,20 +8,9 @@ builder.Services.AddControllers().AddJsonOptions(jsonOptions =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
