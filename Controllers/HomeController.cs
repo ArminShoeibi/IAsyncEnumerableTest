b5e88f69740c6707a0b29d2a815da83208f01b3b
armin <a.shoeibi@alibaba.ir>
2021-11-07 18:46:16 +0330

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAsyncEnumerableTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async IAsyncEnumerable<int> GetNumbersFrom0To1000()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                await Task.Delay(1000);
+                yield return i;
+            }
+        }
+    }
+}
